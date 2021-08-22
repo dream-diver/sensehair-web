010ee40a4373de1ef59a09e6e3a1c953777c6dda
RidwanMalik <skredonemalik@gmail.com>
2021-08-23 00:35:42 +0600

    Top Nav Changes

--- js/scripts.js
+++ js/scripts.js
@@ -150,3 +150,38 @@ const changeBgColorOfTimelineLine = (selector, percent) => {
     })
 }
 changeBgColorOfTimelineLine(".timeline-line", 50);
+
+const visibleOnScroll = (selector) => {
+    percent = 0;
+    // Element Section
+    var element = document.querySelector(selector);
+    var htmlElement = document.querySelector("html");
+    // Listen for the scroll event
+    document.addEventListener('scroll', event => {
+        // Check the viewport status
+        if (element) {
+            if (pxScrolled(htmlElement, percent) > 350) {
+                element.style.transition = 'opacity 750ms ease-in-out';
+                element.style.opacity = '1';
+            } else {
+                element.style.opacity = '0';
+            }
+        }
+    })
+};
+
+visibleOnScroll("#scroll-to-top");
+
+const topOnClick = (selector) => {
+    // Element Section
+    var element = document.querySelector(selector);
+    // Listen for the click event
+    element.addEventListener('click', event => {
+        // Check the viewport status
+        if (element) {
+            document.body.scrollTop = 0; // For Safari
+            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        }
+    });
+}
+topOnClick("#scroll-to-top .btn");
