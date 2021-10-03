230abfaa41248c0669dfc118dd4dc7281e7605fb
ridwanmalik <skredonemalik@gmail.com>
2021-10-03 23:30:17 +0600

    Login Changes

--- components/FloatingWindowAuth.js
+++ components/FloatingWindowAuth.js
@@ -1,6 +1,6 @@
-import { BiRightArrowAlt } from "react-icons/bi"
+import { BiRightArrowAlt, BiShare } from "react-icons/bi"
 
-const FloatingWindowAuth = ({ step, show, setShow, nextStep }) => {
+const FloatingWindowAuth = ({ step, show, setShow, nextStep, skipStep = () => { } }) => {
   return (
     <div className="floating-window">
       <div className="floating-window-header">
@@ -10,13 +10,22 @@ const FloatingWindowAuth = ({ step, show, setShow, nextStep }) => {
       <div className="floating-window-body">
         <form>
           <div className="mb-3">
-            <label htmlFor="inputPhone" className="form-label">Phone</label>
-            <input type="email" className="form-control" id="inputPhone" placeholder={ step.title } />
+            <label htmlFor="inputEmail" className="form-label">Email</label>
+            <input type="email" className="form-control" id="inputEmail" placeholder="Enter your email" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="inputPassword" className="form-label">Password</label>
+            <input type="password" className="form-control" id="inputPassword" placeholder="Enter your password" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="inputPhone" className="form-label">Phone (Optional)</label>
+            <input type="text" className="form-control" id="inputPhone" placeholder="Enter your phone number" />
           </div>
         </form>
       </div>
       <div className="floating-window-footer">
-        <a className="btn-next btn btn-dark" onClick={ nextStep }>Next<BiRightArrowAlt className="ms-1" /></a>
+        <a className="btn-next btn btn-dark" onClick={ skipStep }>Continue as a gust<BiShare className="ms-1 icon-reflect" /></a>
+        <a className="btn-next btn btn-dark" onClick={ nextStep }>Login<BiRightArrowAlt className="ms-1" /></a>
       </div>
     </div>
   )
