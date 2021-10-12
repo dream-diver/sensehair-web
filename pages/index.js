bfe6b56dcc78f830e3c10d65348b8a524b29a88e
Ridwan Malik <skredonemalik@gmail.com>
2021-10-12 16:20:22 +0600

    Add ScrollToTop

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import ScrollToTop from '../components/ScrollToTop'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <ScrollToTop />
       <Footer />
     </div>
   )
