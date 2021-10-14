85afa9c91f180a4b8f1981956de3aed8308d9d52
ridwanmalik <skredonemalik@gmail.com>
2021-10-15 05:14:35 +0600

    Review Slider

--- components/home/Review.js
+++ components/home/Review.js
@@ -1,5 +1,27 @@
 /* eslint-disable @next/next/no-img-element */
+import { useCallback, useEffect, useState } from 'react'
+import useEmblaCarousel from 'embla-carousel-react'
+import DotButton from '../slider/DotButton'
 const Review = () => {
+  const [viewportRef, embla] = useEmblaCarousel({ loop: true, skipSnaps: false })
+  const [scrollSnaps, setScrollSnaps] = useState([]);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const scrollTo = useCallback((index) => embla && embla.scrollTo(index), [
+    embla
+  ]);
+
+  const onSelect = useCallback(() => {
+    if (!embla) return;
+    setSelectedIndex(embla.selectedScrollSnap());
+  }, [embla, setSelectedIndex]);
+
+  useEffect(() => {
+    if (!embla) return;
+    onSelect();
+    setScrollSnaps(embla.scrollSnapList());
+    embla.on("select", onSelect);
+  }, [embla, setScrollSnaps, onSelect]);
+
   return (
     <section id="review" className="text-center py-5">
       <h2 className="quotation" data-aos="fade-up" data-aos-duration="750" data-aos-delay="2500" data-aos-once="true">
@@ -9,53 +31,69 @@ const Review = () => {
 
       <div className="bg-light w-100 pt-5 pb-4">
         <h1 className="h1-margin-bottom font-weight-700" data-aos="fade-up" data-aos-duration="750" data-aos-delay="2500" data-aos-once="true">REVIEWS</h1>
-        <div className="owl-carousel-review owl-carousel owl-theme">
-          <div className="">
-            <div className="container">
-              <div className="row mb-5">
-                <div className="col-6 col-md-4 offset-md-3">
-                  <img loading="lazy" src="./images/rating.png" alt="rating" className="img-fluid" />
-                </div>
-                <div className="col text-start">
-                  <h2 className="review-count d-inline-block font-weight-700 mb-0">658 reviews</h2>
+        <div className="embla">
+          <div className="embla__viewport" ref={ viewportRef }>
+            <div className="embla__container">
+
+
+
+              <div className="embla__slide">
+                <div className="container">
+                  <div className="row mb-5">
+                    <div className="col-6 col-md-4 offset-md-3">
+                      <img loading="lazy" src="./images/rating.png" alt="rating" className="img-fluid" />
+                    </div>
+                    <div className="col text-start">
+                      <h2 className="review-count d-inline-block font-weight-700 mb-0">658 reviews</h2>
+                    </div>
+                  </div>
+
+                  <div className="row">
+                    <div className="col-md-4">
+                      <img loading="lazy" src="./images/image rvw.png" alt="image_rvw" className="img-fluid" />
+                    </div>
+                    <div className="col-md-8 d-flex align-items-center">
+                      <p className="review-text mt-3">“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore cumsan lacus vel facilisis.”</p>
+                    </div>
+                  </div>
                 </div>
               </div>
 
-              <div className="row">
-                <div className="col-md-4">
-                  <img loading="lazy" src="./images/image rvw.png" alt="image_rvw" className="img-fluid" />
-                </div>
-                <div className="col-md-8 d-flex align-items-center">
-                  <p className="review-text mt-3">“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore cumsan lacus vel facilisis.”</p>
+              <div className="embla__slide">
+                <div className="container">
+                  <div className="row mb-5">
+                    <div className="col-6 col-md-4 offset-md-3">
+                      <img loading="lazy" src="./images/rating.png" alt="rating" className="img-fluid" />
+                    </div>
+                    <div className="col text-start">
+                      <h2 className="review-count d-inline-block font-weight-700 mb-0">658 reviews</h2>
+                    </div>
+                  </div>
+
+                  <div className="row">
+                    <div className="col-md-4">
+                      <img loading="lazy" src="./images/image rvw.png" alt="image_rvw" className="img-fluid" />
+                    </div>
+                    <div className="col-md-8 d-flex align-items-center">
+                      <p className="review-text">“Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, impedit!”</p>
+                    </div>
+                  </div>
                 </div>
               </div>
+
+
+
             </div>
           </div>
-
-          <div className="">
-            <div className="container">
-              <div className="row mb-5">
-                <div className="col-4 offset-3">
-                  <img loading="lazy" src="./images/rating.png" alt="rating" className="img-fluid" />
-                </div>
-                <div className="col text-start">
-                  <h2 className="review-count d-inline-block font-weight-700 mb-0">658 reviews</h2>
-                </div>
-              </div>
-
-              <div className="row">
-                <div className="col-md-4">
-                  <img loading="lazy" src="./images/image rvw.png" alt="image_rvw" className="img-fluid" />
-                </div>
-                <div className="col-md-8 d-flex align-items-center">
-                  <p className="review-text">“Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, impedit!”</p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-
-
+        </div>
+        <div className="embla__dots">
+          { scrollSnaps.map((_, index) => (
+            <DotButton
+              key={ index }
+              selected={ index === selectedIndex }
+              onClick={ () => scrollTo(index) }
+            />
+          )) }
         </div>
         <div data-aos="fade-up" data-aos-duration="750" data-aos-delay="3000" data-aos-once="true">
 
--- components/home/Services.js
+++ components/home/Services.js
@@ -1,9 +1,39 @@
 /* eslint-disable @next/next/no-img-element */
-import { useContext } from 'react'
+import { useCallback, useContext, useEffect, useState } from 'react'
 import { GlobalContext } from '../contexts/GlobalContext'
+import useEmblaCarousel from 'embla-carousel-react'
+import DotButton from '../slider/DotButton'
+import PrevButton from '../slider/PrevButton'
+import NextButton from '../slider/NextButton'
+
 
 const Services = () => {
   const [state] = useContext(GlobalContext)
+  const [viewportRef, embla] = useEmblaCarousel({ skipSnaps: false });
+  const [prevBtnEnabled, setPrevBtnEnabled] = useState(false);
+  const [nextBtnEnabled, setNextBtnEnabled] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [scrollSnaps, setScrollSnaps] = useState([]);
+
+  const scrollPrev = useCallback(() => embla && embla.scrollPrev(), [embla]);
+  const scrollNext = useCallback(() => embla && embla.scrollNext(), [embla]);
+  const scrollTo = useCallback((index) => embla && embla.scrollTo(index), [
+    embla
+  ]);
+
+  const onSelect = useCallback(() => {
+    if (!embla) return;
+    setSelectedIndex(embla.selectedScrollSnap());
+    setPrevBtnEnabled(embla.canScrollPrev());
+    setNextBtnEnabled(embla.canScrollNext());
+  }, [embla, setSelectedIndex]);
+
+  useEffect(() => {
+    if (!embla) return;
+    onSelect();
+    setScrollSnaps(embla.scrollSnapList());
+    embla.on("select", onSelect);
+  }, [embla, setScrollSnaps, onSelect]);
 
   return (
     <section id="services">
@@ -47,39 +77,54 @@ const Services = () => {
               </div>
             </div>
             <div className="d-md-none">
-              <div className="owl-carousel-services owl-carousel owl-theme">
-                <div className="">
-                  <div className="img-overlay">
-                    <img loading="lazy" src="./images/Haircut.png" alt="Haircut" className="img-fluid" />
-                    <div className="overlay">
-                      <h1 className="mb-0">Haircut</h1>
+              <div className="embla">
+                <div className="embla__viewport" ref={ viewportRef }>
+                  <div className="embla__container">
+                    <div className="embla__slide">
+                      <div className="img-overlay">
+                        <img loading="lazy" src="./images/Haircut.png" alt="Haircut" className="img-fluid" />
+                        <div className="overlay">
+                          <h1 className="mb-0">Haircut</h1>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="embla__slide">
+                      <div className="img-overlay">
+                        <img loading="lazy" src="./images/Styling.png" alt="Styling" className="img-fluid" />
+                        <div className="overlay">
+                          <h1 className="mb-0">Styling</h1>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="embla__slide">
+                      <div className="img-overlay">
+                        <img loading="lazy" src="./images/Color.png" alt="Color" className="img-fluid" />
+                        <div className="overlay">
+                          <h1 className="mb-0">Color</h1>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="embla__slide">
+                      <div className="img-overlay">
+                        <img loading="lazy" src="./images/Color.png" alt="Color" className="img-fluid" />
+                        <div className="overlay">
+                          <h1 className="mb-0">Color</h1>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
-                <div className="">
-                  <div className="img-overlay">
-                    <img loading="lazy" src="./images/Styling.png" alt="Styling" className="img-fluid" />
-                    <div className="overlay">
-                      <h1 className="mb-0">Styling</h1>
-                    </div>
-                  </div>
-                </div>
-                <div className="">
-                  <div className="img-overlay">
-                    <img loading="lazy" src="./images/Color.png" alt="Color" className="img-fluid" />
-                    <div className="overlay">
-                      <h1 className="mb-0">Color</h1>
-                    </div>
-                  </div>
-                </div>
-                <div className="">
-                  <div className="img-overlay">
-                    <img loading="lazy" src="./images/Color.png" alt="Color" className="img-fluid" />
-                    <div className="overlay">
-                      <h1 className="mb-0">Color</h1>
-                    </div>
-                  </div>
-                </div>
+                <PrevButton onClick={ scrollPrev } enabled={ prevBtnEnabled } />
+                <NextButton onClick={ scrollNext } enabled={ nextBtnEnabled } />
+              </div>
+              <div className="embla__dots">
+                { scrollSnaps.map((_, index) => (
+                  <DotButton
+                    key={ index }
+                    selected={ index === selectedIndex }
+                    onClick={ () => scrollTo(index) }
+                  />
+                )) }
               </div>
             </div>
 
