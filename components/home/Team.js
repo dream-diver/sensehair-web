8c12780d48cdc173de56dd9af3b836b81d986334
ridwanmalik <skredonemalik@gmail.com>
2021-10-15 04:03:36 +0600

    Team Slider

--- components/home/Team.js
+++ components/home/Team.js
@@ -1,45 +1,90 @@
 /* eslint-disable @next/next/no-img-element */
-import { useContext } from 'react'
+import { useCallback, useContext, useEffect, useState } from 'react'
 import { GlobalContext } from '../contexts/GlobalContext'
+import useEmblaCarousel from 'embla-carousel-react'
 
 const Team = () => {
   const [state] = useContext(GlobalContext)
+  const [viewportRef, embla] = useEmblaCarousel({ skipSnaps: false })
+  const [scrollSnaps, setScrollSnaps] = useState([]);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const scrollTo = useCallback((index) => embla && embla.scrollTo(index), [
+    embla
+  ]);
+
+  const onSelect = useCallback(() => {
+    if (!embla) return;
+    setSelectedIndex(embla.selectedScrollSnap());
+  }, [embla, setSelectedIndex]);
+
+  useEffect(() => {
+    if (!embla) return;
+    onSelect();
+    setScrollSnaps(embla.scrollSnapList());
+    embla.on("select", onSelect);
+  }, [embla, setScrollSnaps, onSelect]);
 
   return (
     <section id="team" className="py-5 text-center">
       <div>
-        <div className="owl-carousel-team owl-carousel owl-theme">
-          <div className="row gx-0 text-white">
-            <div className="col-md-6">
-              <img loading="lazy" src="./images/team 1.png" alt="team 1" className="img-fluid" />
-            </div>
-            <div className="col-md-6 bg-black d-flex flex-column justify-content-center align-items-center pt-5 pt-md-3 py-3 px-2 px-md-5">
-              <div className="team-text flex-grow-1 d-flex align-items-center justify-content-center flex-column">
-                <h1 className="font-weight-700">{ state.text.homeTeamTeam1Title }</h1>
-                <p className="mb-4">{ state.text.homeTeamTeam1Body }</p>
-                <button className="btn btn-light btn-lg font-weight-900 text-uppercase">{ state.text.homeTeamTeam1Button }</button>
-              </div>
-              <p className="font-handwriting my-3">Carlos Ramos</p>
-            </div>
-          </div>
+        <div className="embla">
+          <div className="embla__viewport" ref={ viewportRef }>
+            <div className="embla__container">
 
-          <div className="row gx-0 text-white">
-            <div className="col-md-6">
-              <img loading="lazy" src="./images/team 1.png" alt="team 1" className="img-fluid" />
-            </div>
-            <div className="col-md-6 bg-black d-flex flex-column justify-content-center align-items-center py-3 px-5">
-              <div className="team-text flex-grow-1 d-flex align-items-center justify-content-center flex-column">
-                <h1 className="font-weight-700">{ state.text.homeTeamTeam2Title }</h1>
-                <p className="mb-4">{ state.text.homeTeamTeam2Body }</p>
-                <button className="btn btn-light btn-lg font-weight-900 text-uppercase">{ state.text.homeTeamTeam2Button }</button>
+              <div className="embla__slide">
+                <div className=" row gx-0 text-white">
+                  <div className="col-md-6">
+                    <img loading="lazy" src="./images/team 1.png" alt="team 1" className="img-fluid" />
+                  </div>
+                  <div className="col-md-6 bg-black d-flex flex-column justify-content-center align-items-center pt-5 pt-md-3 py-3 px-2 px-md-5">
+                    <div className="team-text flex-grow-1 d-flex align-items-center justify-content-center flex-column">
+                      <h1 className="font-weight-700">{ state.text.homeTeamTeam1Title }</h1>
+                      <p className="mb-4">{ state.text.homeTeamTeam1Body }</p>
+                      <button className="btn btn-light btn-lg font-weight-900 text-uppercase">{ state.text.homeTeamTeam1Button }</button>
+                    </div>
+                    <p className="font-handwriting my-3">Carlos Ramos</p>
+                  </div>
+                </div>
               </div>
-              <p className="font-handwriting my-3">Hande Erçel</p>
+
+              <div className="embla__slide">
+                <div className="row gx-0 text-white">
+                  <div className="col-md-6">
+                    <img loading="lazy" src="./images/team 1.png" alt="team 1" className="img-fluid" />
+                  </div>
+                  <div className="col-md-6 bg-black d-flex flex-column justify-content-center align-items-center py-3 px-5">
+                    <div className="team-text flex-grow-1 d-flex align-items-center justify-content-center flex-column">
+                      <h1 className="font-weight-700">{ state.text.homeTeamTeam2Title }</h1>
+                      <p className="mb-4">{ state.text.homeTeamTeam2Body }</p>
+                      <button className="btn btn-light btn-lg font-weight-900 text-uppercase">{ state.text.homeTeamTeam2Button }</button>
+                    </div>
+                    <p className="font-handwriting my-3">Hande Erçel</p>
+                  </div>
+                </div>
+              </div>
+
             </div>
           </div>
+        </div>
+        <div className="embla__dots">
+          { scrollSnaps.map((_, index) => (
+            <DotButton
+              key={ index }
+              selected={ index === selectedIndex }
+              onClick={ () => scrollTo(index) }
+            />
+          )) }
         </div>
       </div>
     </section>
   )
 }
+const DotButton = ({ selected, onClick }) => (
+  <button
+    className={ `embla__dot ${selected ? "is-selected" : ""}` }
+    type="button"
+    onClick={ onClick }
+  />
+);
 
 export default Team
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import Script from 'next/script'
 import { GlobalProvider } from "../components/contexts/GlobalContext";
-// import 'owl.carousel/dist/assets/owl.carousel.min.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
 import '../styles/responsive.css'
@@ -10,9 +9,6 @@ function MyApp({ Component, pageProps }) {
     <GlobalProvider>
       <Component { ...pageProps } />
       <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" strategy="lazyOnload" />
-      <Script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" strategy="lazyOnload" />
-      <Script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" strategy="lazyOnload" />
-      <Script src="../coustom/scripts.js" strategy="lazyOnload" />
     </GlobalProvider>
   )
 }
