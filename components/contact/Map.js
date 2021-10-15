9f4f39d6bf4a51633def478ce79b542202859ac4
Ridwan Malik <skredonemalik@gmail.com>
2021-10-15 18:48:09 +0600

    Map allowFullScreen Property Error

--- components/contact/Map.js
+++ components/contact/Map.js
@@ -1,7 +1,7 @@
 const Map = () => {
   return (
     <section id="map">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2460.599838368103!2d4.470254515785334!3d51.9230115797056!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c434b9335c794b%3A0x3104eee3db8dcbbd!2sSense!5e0!3m2!1sen!2sbd!4v1631422130551!5m2!1sen!2sbd" width="100%" height="555" style={ { "border": 0 } } allowfullscreen="" loading="lazy"></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2460.599838368103!2d4.470254515785334!3d51.9230115797056!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c434b9335c794b%3A0x3104eee3db8dcbbd!2sSense!5e0!3m2!1sen!2sbd!4v1631422130551!5m2!1sen!2sbd" width="100%" height="555" style={ { "border": 0 } } allowFullScreen="" loading="lazy"></iframe>
     </section >
   )
 }
