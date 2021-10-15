f1347bb86dedeacd0ed58b28978178ceba57da59
Ridwan Malik <skredonemalik@gmail.com>
2021-10-15 12:31:52 +0600

    InfoLists Styling

--- components/career/InfoList.js
+++ components/career/InfoList.js
@@ -10,12 +10,12 @@ const InfoList = () => {
         <div className="row">
           <div className="col py-5" data-aos="fade-up" data-aos-duration="750" data-aos-delay="500" data-aos-once="true">
             <h1 className="font-weight-700 mb-3">{ state.text.careerInfoListTitle }</h1>
-            <ul className="font-1-5x">
-              <li>{ state.text.careerInfoListItem1 }</li>
-              <li>{ state.text.careerInfoListItem2 }</li>
-              <li>{ state.text.careerInfoListItem3 }</li>
-              <li>{ state.text.careerInfoListItem4 }</li>
-              <li>{ state.text.careerInfoListItem5 }</li>
+            <ul className="font-1-3x">
+              <li className="mb-3">{ state.text.careerInfoListItem1 }</li>
+              <li className="mb-3">{ state.text.careerInfoListItem2 }</li>
+              <li className="mb-3">{ state.text.careerInfoListItem3 }</li>
+              <li className="mb-3">{ state.text.careerInfoListItem4 }</li>
+              <li className="mb-3">{ state.text.careerInfoListItem5 }</li>
               <li>{ state.text.careerInfoListItem6 }</li>
             </ul>
           </div>
--- components/career/InfoListText.js
+++ components/career/InfoListText.js
@@ -9,11 +9,11 @@ const InfoListText = () => {
         <div className="row">
           <div className="col py-5" data-aos="fade-up" data-aos-duration="750" data-aos-delay="500" data-aos-once="true">
             <h1 className="font-weight-700 mb-3">{ state.text.careerInfoListTextTitle }</h1>
-            <ul className="font-1-5x">
-              <li>{ state.text.careerInfoListTextItem1 }</li>
-              <li>{ state.text.careerInfoListTextItem2 }</li>
-              <li>{ state.text.careerInfoListTextItem3 }</li>
-              <li>{ state.text.careerInfoListTextItem4 }</li>
+            <ul className="font-1-3x">
+              <li className="mb-3">{ state.text.careerInfoListTextItem1 }</li>
+              <li className="mb-3">{ state.text.careerInfoListTextItem2 }</li>
+              <li className="mb-3">{ state.text.careerInfoListTextItem3 }</li>
+              <li className="mb-3">{ state.text.careerInfoListTextItem4 }</li>
               <li>{ state.text.careerInfoListTextItem5 }</li>
             </ul>
             <p className="font-1-3x">{ state.text.careerInfoListTextBody }</p>
