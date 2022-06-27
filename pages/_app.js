e7318da24bdf00a39396fe489fc814fa25aa2815
Rashiq Rahman <33522666+Rashiq50@users.noreply.github.com>
2022-06-27 22:12:13 +0600

    script id

--- pages/_app.js
+++ pages/_app.js
@@ -42,10 +42,9 @@ function MyApp({ Component, pageProps }) {
         transition={Slide}
       />
       <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" strategy="lazyOnload" />
-      <Script strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></Script>
+      <Script id='tag1' strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></Script>
       <Script id='analytic'>
         {
-
           `window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
@@ -54,7 +53,7 @@ function MyApp({ Component, pageProps }) {
         }
       </Script>
 
-      <Script strategy="afterInteractive" dangerouslySetInnerHTML={{
+      <Script id='tag' strategy="afterInteractive" dangerouslySetInnerHTML={{
         __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
