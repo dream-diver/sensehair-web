fdc4f880e48bffcdb2c0ff10862381166a0f287a
riaznur <nuruddinriaz5@gmail.com>
2022-12-13 15:54:29 +0600

    parse float price

--- components/Helpers.js
+++ components/Helpers.js
@@ -1,30 +1,35 @@
 export const pxScrolled = (element, percent) => {
   // Get the elements position relative to the viewport
-  var bb = element.getBoundingClientRect()
+  var bb = element.getBoundingClientRect();
   // Check if the element is outside the viewport
   // Then invert the returned value because you want to know the opposite
-  return -(bb.top - (innerHeight - (window.innerHeight / (100 / (100 - percent)))))
-}
+  return -(
+    bb.top -
+    (innerHeight - window.innerHeight / (100 / (100 - percent)))
+  );
+};
 
 // Detect Element In Viewport Or Not For Image Background Section
 export const inViewport = (element, percent) => {
   // Get the elements position relative to the viewport
-  var bb = element.getBoundingClientRect()
+  var bb = element.getBoundingClientRect();
   // Check if the element is outside the viewport
   // Then invert the returned value because you want to know the opposite
-  return !(bb.top > innerHeight - (window.innerHeight / (100 / (100 - percent))))
-}
+  return !(bb.top > innerHeight - window.innerHeight / (100 / (100 - percent)));
+};
 
 // fetch Services
 export const fetchServices = async (hairSize, hairType) => {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/services?hair_size=${hairSize}&hair_type=${hairType}&limit=all`)
-    const data = await response.json()
-    return data
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/services?hair_size=${hairSize}&hair_type=${hairType}&limit=all`
+    );
+    const data = await response.json();
+    return data;
   } catch (error) {
-    console.log(error.message)
-    return null
+    console.log(error.message);
+    return null;
   }
-}
+};
 // Make Price
-export const makePrice = price => `€${parseInt(price).toFixed(2)}`
+export const makePrice = (price) => `€${parseFloat(price).toFixed(2)}`;
